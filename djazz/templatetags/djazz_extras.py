52765c22caac7e2de3198bdde554e607e1f2d7b0
Guillaume Dugas <dugas.guillaume@gmail.com>
2013-01-25 18:07:53 +0100

    Adding attrof and indexof template filters

--- djazz/templatetags/djazz_extras.py
+++ djazz/templatetags/djazz_extras.py
@@ -19,3 +19,15 @@ def filter_range(value, arg="0, 1"):
         step = int(args.pop().strip())
     
     return range(start, stop, step)
+
+
+@register.filter(name='indexof')
+def filter_index(index, tab):
+    if type(tab) == list and len(tab) > index:
+        return tab[index]
+    else:
+        return None
+
+@register.filter(name='attrof')
+def filter_attr(attr, obj):
+    return getattr(obj, attr, None)
