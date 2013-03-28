98b143d11e0ee8c9fadfcd3d164d8b45bb200466
Guillaume Dugas <dugas.guillaume@gmail.com>
2013-03-28 17:28:35 +0100

    Updating config admin ordering

--- djazz/admin.py
+++ djazz/admin.py
@@ -6,5 +6,6 @@ class AdminConfig(admin.ModelAdmin):
     list_display = ('section', 'key', 'value')
     list_filter = ('section',)
     search_fields = ['key', 'value']
+    ordering = ('section', 'key',)
 
 admin.site.register(Config, AdminConfig)
